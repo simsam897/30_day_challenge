b8a3513e023733203d3b7c9f17c9f1892fb82bd0
simsam897 <simma013s@gmail.com>
2024-07-28 23:18:06 +0530

    day16 recursion

--- day15.js
+++ day15.js
@@ -23,7 +23,7 @@ console.log(counter());
 console.log(counter());
 
 function idgenerator() {
-  let uniqueId = 1121;
+  let uniqueId = 6555;
   return function () {
     uniqueId = uniqueId + 1;
     return uniqueId;
@@ -57,75 +57,65 @@ for (let i = 0; i < 10; i++) {
   })(i);
 }
 
-
-functionsArray[0](); 
+functionsArray[0]();
 functionsArray[1]();
-functionsArray[2](); 
-functionsArray[3](); 
+functionsArray[2]();
+functionsArray[3]();
 
 function createCollectionManager() {
-  
   let items = [];
 
   return {
-      
-      addItem: function(item) {
-          items.push(item);
-          console.log(`${item} has been added.`);
-      },
-      
-      
-      removeItem: function(item) {
-          const index = items.indexOf(item);
-          if (index !== -1) {
-              items.splice(index, 1);
-              console.log(`${item} has been removed.`);
-          } else {
-              console.log(`${item} not found in the collection.`);
-          }
-      },
-      
-      
-      listItems: function() {
-          if (items.length === 0) {
-              console.log('The collection is empty.');
-          } else {
-              console.log('Items in the collection:');
-              items.forEach(item => console.log(item));
-          }
+    addItem: function (item) {
+      items.push(item);
+      console.log(`${item} has been added.`);
+    },
+
+    removeItem: function (item) {
+      const index = items.indexOf(item);
+      if (index !== -1) {
+        items.splice(index, 1);
+        console.log(`${item} has been removed.`);
+      } else {
+        console.log(`${item} not found in the collection.`);
       }
+    },
+
+    listItems: function () {
+      if (items.length === 0) {
+        console.log("The collection is empty.");
+      } else {
+        console.log("Items in the collection:");
+        items.forEach((item) => console.log(item));
+      }
+    },
   };
 }
 
 const myCollection = createCollectionManager();
-myCollection.addItem('Book');
-myCollection.addItem('Pen');
-myCollection.listItems(); 
-myCollection.removeItem('Pen');
-myCollection.listItems(); 
-myCollection.removeItem('Pencil'); 
-
-
-
-
+myCollection.addItem("Book");
+myCollection.addItem("Pen");
+myCollection.listItems();
+myCollection.removeItem("Pen");
+myCollection.listItems();
+myCollection.removeItem("Pencil");
 
 function memoize(fn) {
   const cache = {};
 
-  return function(...args) {
-      const key = JSON.stringify(args);
-      if (cache[key]) {
-          console.log('Returning cached result for', args);
-          return cache[key];
-      } else {
-          const result = fn(...args);
-          cache[key] = result;
-          console.log('Computing new result for', args);
-          return result;
-      }
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache[key]) {
+      console.log("Returning cached result for", args);
+      return cache[key];
+    } else {
+      const result = fn(...args);
+      cache[key] = result;
+      console.log("Computing new result for", args);
+      return result;
+    }
   };
 }
-
 
 function add(a, b) {
   return a + b;
@@ -133,40 +123,38 @@ function add(a, b) {
 
 const memoizedAdd = memoize(add);
 
-console.log(memoizedAdd(1, 2)); 
-console.log(memoizedAdd(1, 2)); 
-console.log(memoizedAdd(2, 3)); 
-console.log(memoizedAdd(2, 3)); 
-
-
+console.log(memoizedAdd(1, 2));
+console.log(memoizedAdd(1, 2));
+console.log(memoizedAdd(2, 3));
+console.log(memoizedAdd(2, 3));
 
 function memoize(fn) {
   const cache = {};
 
-  return function(...args) {
-      const key = JSON.stringify(args);
-      if (cache[key] !== undefined) {
-          console.log('Returning cached result for', args);
-          return cache[key];
-      } else {
-          const result = fn(...args);
-          cache[key] = result;
-          console.log('Computing new result for', args);
-          return result;
-      }
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache[key] !== undefined) {
+      console.log("Returning cached result for", args);
+      return cache[key];
+    } else {
+      const result = fn(...args);
+      cache[key] = result;
+      console.log("Computing new result for", args);
+      return result;
+    }
   };
 }
 
 function factorial(n) {
   if (n === 0 || n === 1) {
-      return 1;
+    return 1;
   }
   return n * factorial(n - 1);
 }
 
 const memoizedFactorial = memoize(factorial);
 
-console.log(memoizedFactorial(5)); 
-console.log(memoizedFactorial(5)); 
-console.log(memoizedFactorial(6)); 
-console.log(memoizedFactorial(6)); 
+console.log(memoizedFactorial(5));
+console.log(memoizedFactorial(5));
+console.log(memoizedFactorial(6));
+console.log(memoizedFactorial(6));
---
+++ day16.js
@@ -0,0 +1,160 @@
+function factorial(num) {
+  if (num < 0) {
+    console.log(`factoial does not define the negative numbers`);
+  }
+  if (num === 0 || num === 1) {
+    console.log(`factorial ${num} is 1`);
+    return 1;
+  } else {
+    const result = num * factorial(num - 1);
+    console.log(`factorial (${num} )= ${result}`);
+    return result;
+  }
+}
+
+const num = 8;
+
+console.log(`The factorial of ${num} is ${factorial(num)}`);
+
+function fibonacci(n) {
+  if (n < 0) {
+    throw new Error("Fibonacci is not defined for negative numbers");
+  }
+  let a = 0,
+    b = 1,
+    temp;
+  if (n === 0) {
+    return a;
+  }
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
+
+const n = 6;
+console.log(`The ${n}th Fibonacci number is ${fibonacci(n)}`);
+
+function sumArray(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr[0] + sumArray(arr.slice(1));
+}
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(sumArray([15, 13, 67, 87, 88]));
+console.log(sumArray([14, 76, 98, 65, 43, 99]));
+
+function logSum(arr) {
+  console.log(`The sum of [${arr}] is ${sumArray(arr)}`);
+}
+
+console.log(logSum([1, 2, 3, 4, 5]));
+console.log(logSum([15, 13, 67, 87, 88]));
+console.log(logSum([14, 76, 98, 65, 43, 99]));
+
+function findMax(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return Math.max(arr[0], findMax(arr.slice(1)));
+}
+
+console.log(findMax([1, 2, 3, 4, 5]));
+
+function logMax(arr) {
+  console.log(`The maximum element in [${arr}] is ${findMax(arr)}`);
+}
+
+logMax([1, 2, 3, 4, 5]);
+
+function reverseString(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  return str[str.length - 1] + reverseString(str.slice(0, -1));
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString("!gnirts esrever"));
+console.log(reverseString("a"));
+console.log(reverseString(""));
+
+function isPalindrome(str) {
+  if (str.length <= 1) {
+    return true;
+  }
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+  return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("level"));
+
+function logPalindromeCheck(str) {
+  const result = isPalindrome(str);
+  console.log(`Is "${str}" a palindrome? ${result}`);
+}
+
+logPalindromeCheck("racecar");
+logPalindromeCheck("level");
+
+function binarySearch(arr, target, left = 0, right = arr.length - 1) {
+  if (left > right) {
+    return -1;
+  }
+  const mid = Math.floor((left + right) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  }
+  if (arr[mid] > target) {
+    return binarySearch(arr, target, left, mid - 1);
+  }
+  return binarySearch(arr, target, mid + 1, right);
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 3));
+console.log(binarySearch([10, 20, 30, 40, 50], 40));
+
+function logBinarySearch(arr, target) {
+  const result = binarySearch(arr, target);
+  console.log(`The index of ${target} in [${arr}] is ${result}`);
+}
+
+logBinarySearch([1, 2, 3, 4, 5], 3);
+logBinarySearch([10, 20, 30, 40, 50], 40);
+
+function countOccurrences(arr, target, index = 0) {
+  if (index >= arr.length) {
+    return 0;
+  }
+  const match = arr[index] === target ? 1 : 0;
+  return match + countOccurrences(arr, target, index + 1);
+}
+
+console.log(countOccurrences([1, 2, 3, 4, 5, 3, 3], 3));
+console.log(countOccurrences([10, 20, 30, 40, 50, 20], 20));
+console.log(countOccurrences([-1, -2, -3, -4, -5], -3));
+console.log(countOccurrences([1, 2, 3, 4, 5], 6));
+console.log(countOccurrences([], 1));
+console.log(countOccurrences([1, 1, 1, 1, 1], 1));
+
+function logCountOccurrences(arr, target) {
+  const result = countOccurrences(arr, target);
+  console.log(
+    `The number of occurrences of ${target} in [${arr}] is ${result}`
+  );
+}
+
+logCountOccurrences([1, 2, 3, 4, 5, 3, 3], 3);
+logCountOccurrences([10, 20, 30, 40, 50, 20], 20);
+logCountOccurrences([-1, -2, -3, -4, -5], -3);
+logCountOccurrences([1, 2, 3, 4, 5], 6);
+logCountOccurrences([], 1);
+logCountOccurrences([1, 1, 1, 1, 1], 1);
